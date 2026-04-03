db0f4b8daf39f8de665fadac18d44245905a41d6
siamubee2-max <siamubee2@gmail.com>
2026-04-03 14:35:29 +0200

    fix: force iOS buildNumber to 13 (ASC has build 12 from Feb, need > 12)

--- app.config.ts
+++ app.config.ts
@@ -39,6 +39,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
+    buildNumber: "13",
     // Associated domains for Universal Links
     associatedDomains: [
       `applinks:${DEEP_LINK_CONFIG.associatedDomain}`,
